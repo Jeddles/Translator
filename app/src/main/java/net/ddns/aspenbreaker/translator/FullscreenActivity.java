3a585479195b409a69a88fe7ad6c35f9aa137de1
Jedd Orsmond <orsmond.3@gmail.com>
2020-03-24 15:12:31 +0200

    said hello world bruh

--- app/src/main/java/net/ddns/aspenbreaker/translator/FullscreenActivity.java
+++ app/src/main/java/net/ddns/aspenbreaker/translator/FullscreenActivity.java
@@ -2,6 +2,7 @@ package net.ddns.aspenbreaker.translator;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 
 public class FullscreenActivity extends AppCompatActivity
@@ -11,6 +12,8 @@ public class FullscreenActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fullscreen);
+
+        Log.i("tag", "hello, World!");
     }
 
 }

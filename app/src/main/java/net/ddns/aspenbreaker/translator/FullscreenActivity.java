58c2c425c99c276c6cbdd9202e1db4ae12bfaed9
Jedd Orsmond <orsmond.3@gmail.com>
2020-03-24 15:45:42 +0200

    asdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdsadasdsdadasdasdasddsadasdsadsadasdsadasdaasdasdasd

--- app/src/main/java/net/ddns/aspenbreaker/translator/FullscreenActivity.java
+++ app/src/main/java/net/ddns/aspenbreaker/translator/FullscreenActivity.java
@@ -14,6 +14,7 @@ public class FullscreenActivity extends AppCompatActivity
         setContentView(R.layout.activity_fullscreen);
 
         Log.i("tag", "hello, World!");
+        Log.i("tag", "hello, World!");
     }
 
 }
